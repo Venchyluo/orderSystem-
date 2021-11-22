49e3df912b9085c73188d6922adc06f0a22edac9
James Zhang <yixuan.zhang@microsoft.com>
2021-11-22 22:31:13 +0800

    add express server

--- README.md
+++ README.md
@@ -1,1 +1,14 @@
 # orderSystem-
+
+saveContact(phoneNumber, address)
+POST
+Return: success/error
+
+
+getAllPhoneNumbers()
+GET
+Return: [phoneNumber]
+
+getAddressByPhoneNumber(phoneNumber)
+GET
+Return: address
--- ordersystem/src/constants.js
+++ ordersystem/src/constants.js
@@ -1,5 +1,5 @@
 // export const API_ROOT = 'https://www.deployedpage.com:8080';
-export const API_ROOT = "http://localhost:8080/api";
+export const API_ROOT = "http://localhost:5000/api";
 //local development
 export const ROOT = "http://localhost:3000";
 
